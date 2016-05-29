64167bc526fdb53af8b0f8757257b5824d24e6ce
Broc Nickodemus <nickodeb@oregonstate.edu>
2016-05-28 22:36:01 -0700

    tkinter

--- blackjack/bj.py
+++ blackjack/bj.py
@@ -1,9 +1,6 @@
-try:
-    # for Python2
-    from Tkinter import *   ## notice capitalized T in Tkinter 
-except ImportError:
-    # for Python3
-    from tkinter import *   ## notice here too
+from tkinter import *
+from tkinter import ttk
+#from PIL import ImageTk, Image
 
 import random
 from random import shuffle
@@ -11,16 +8,19 @@ from random import shuffle
 SUIT = ['C','D','S','H']
 TYPE = ['A','2','3','4','5','6','7','8','9','J','Q','K']
 VALUE = {'A':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'J':10,'Q':10,'K':10}
+IMAGE = ['ac.gif','2c.gif','3c.gif','4c.gif','5c.gif','6c.gif','7c.gif','8c.gif','9c.gif','10c.gif','jc.gif','qc.gif','kc.gif']
 
 class Card():
 
-    def __init__(self,type=None,suit=None):
+    def __init__(self,type=None,suit=None,image=None):
         if (type or suit) is None:
             self.type = 'X'
             self.suit = 'X'
+            self.image = "ac.gif"
         else:
             self.type = type
             self.suit = suit
+            self.image = image
     def __str__(self):
         return "{0.type} of {0.suit}".format(self)
     def getSuit(self):
@@ -34,7 +34,7 @@ class Deck():
         self.deck = []
         for i in range(0,4):
             for j in range(0,12):
-                c = Card(TYPE[j],SUIT[i])
+                c = Card(TYPE[j],SUIT[i],IMAGE[j])
                 self.deck.append(c)
     def __str__(self):
         s = ''
@@ -72,6 +72,7 @@ class Player():
                 choice = input("[H]it [S]tand ").lower()
                 while choice not in ['H','h','S','s']:
                     print("invalid input")
+                    
                     choice = input("would you like to hit or stand > ")
                 if choice == 'h':
                     self.hand.append(deck.pop())
@@ -120,11 +121,23 @@ def init():
     d.shuffle()
     p1 = Player()
     dealer = Dealer()
+    
     p1.add_card(d.pop())
     p1.add_card(d.pop())
+    
+    print(p1.hand[0].image)
+    print('\n'+'\n')
+    x=40
+    gif1 = PhotoImage(file = p1.hand[0].image)
+    canvas.create_image(x, 200, image = gif1, anchor = NW)
+    x+=40
+    gif2 = PhotoImage(file = p1.hand[1].image)
+    canvas.create_image(x, 200, image = gif1, anchor = NW)
     print("cards in your hand:" + "\n" + str(p1) + "total:" + str(p1.getValue()) + '\n')
+   
     dealer.add_card(d.pop())
     dealer.add_card(d.pop())
+    
     print("dealers hand:" + '\n' + str(dealer.hand[0]) + '\n' + "*")
 
 def game():
@@ -142,6 +155,38 @@ def game():
         print("push")
     else:
         print("you lose")
+
+class App:
+    master = Tk()
+
+    content = Canvas(master)
+    canvas = Canvas(content, borderwidth=5, height=400, width=600, background="Dark Green")
+    canvas.pack(expand = YES, fill = BOTH)
+    #frame.pack_propagate(0) # don't shrink
+
+    def hitCallback():
+        print ("hit!")
+    def standCallback():
+        print ("stand!")
+    
+    def printCard(self, card):
+        x=400
+        img = PhotoImage(file = card.image)
+        canvas.create_image(x, 200, image = img, anchor = NW)
+    
+    hit = Button(content, text="hit", command=hitCallback)
+    stand = Button(content, text="stand", command=standCallback)
+    content.grid(column=0, row=0)
+    canvas.grid(column=0, row=0, columnspan=6, rowspan=6)
+    hit.grid(column=0, row=5)
+    stand.grid(column=1, row=5)
+
+    x = 100
+    gif3 = PhotoImage(file = 'jc.gif')
+    canvas.create_image(x, 200, image = gif3, anchor = NW)
+
+    master.mainloop()
+
 
 game()
 
